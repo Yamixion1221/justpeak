c99f68c91f5eaa78a9da5467969a06dc214079f5
Yamixion1221 <yamixion@gmail.com>
2025-06-28 15:14:22 +0700

    Create Scriptmej.js

---
+++ Scriptmej.js
@@ -0,0 +1,118 @@
+// проверка доступности cookie:
+function areCookiesEnabled() {
+    var cookieEnabled = navigator.cookieEnabled;
+    if (cookieEnabled === undefined) {
+        document.cookie = "testcookie";
+        cookieEnabled = document.cookie.indexOf("testcookie") != -1;
+    }
+    return cookieEnabled;
+}
+if (!areCookiesEnabled()) {
+var cookieoff = 1;
+} else {
+var cookieoff = 0;
+}
+
+if (window.location.hostname !== window.atob("d3d3Lmt1eWhhYS5tZQ==") && window.location.hostname !== window.atob("")) {
+window.location = window.atob("aHR0cHM6Ly93d3cua3V5aGFhLm1lLw==");
+throw "stop";
+}
+   
+function b64_to_utf8(str) {
+str = str.replace(/\s/g, '');    
+return decodeURIComponent(escape(window.atob(str)));
+}
+
+document.getElementById("content").innerHTML = "Memuat..."; //
+
+function asyncFunction1() {
+  return new Promise(function(resolve) {
+rct = ''; //
+resolve('Result of Async Function 1');
+  });
+}
+
+function asyncFunction2() {
+  return new Promise(function(resolve) {
+ipv4 = '';
+ipdbc = '';
+resolve('Result of Async Function 2');
+  });
+}
+
+
+// c6290ad628be897c556c6ff7cb4cf70b
+function anotherFunction(result1, result2) {
+data = 'useragent=Mozilla%2F5.0+%28Windows+NT+10.0%3B+Win64%3B+x64%29+AppleWebKit%2F537.36+%28KHTML%2C+like+Gecko%29+Chrome%2F137.0.0.0+Safari%2F537.36&test=889c3f50aa39b03ddc39543e747d0521c9f076672a3fc0d792221188dcf4f9ec&h1=5ab8198c6349e89ccf5d1a65622c3a220a47e5e9a39a7687e92f70c0d89e29d3&date=1751097947&hdc=0&a='+adb+'&country=KH&ip=103.136.23.89&v=9.064&cid=1751097947.7851&ptr=103.136.23.89&w='+screen.width+'&h='+screen.height+'&cw='+document.documentElement.clientWidth+'&ch='+document.documentElement.clientHeight+'&co='+screen.colorDepth+'&pi='+screen.pixelDepth+'&ref='+encodeURIComponent(document.referrer)+'&accept=text%2Fhtml%2Capplication%2Fxhtml%2Bxml%2Capplication%2Fxml%3Bq%3D0.9%2Cimage%2Favif%2Cimage%2Fwebp%2Cimage%2Fapng%2C%2A%2F%2A%3Bq%3D0.8%2Capplication%2Fsigned-exchange%3Bv%3Db3%3Bq%3D0.7&tz='+Intl.DateTimeFormat().resolvedOptions().timeZone+'&ipdbc='+ipdbc+'&ipv4='+ipv4+'&rct='+rct+'&cookieoff='+cookieoff;
+f0e13e42b3896f504d5ecee88196641e9('ab', data, '');
+  console.log('Another Function executed with results:', result1, result2);
+}
+
+async function runAsyncFunctions() {
+  try {
+    const result1 = await asyncFunction1();
+    const result2 = await asyncFunction2();
+    anotherFunction(result1, result2);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+runAsyncFunctions();
+
+
+
+function Button() {
+
+document.getElementById("content").innerHTML = "<div class=\"s823aef64de972b3e453f8f787e10db94\" style=\"cursor: none; pointer-events: none; background-color: #ea0000;\" /></div><p>Jika Anda adalah manusia, klik warna yang serupa</p><p><b class=\"xxNhWElLHdASoGRv s823aef64de972b3e453f8f787e10db94\" onclick=\"f0e13e42b3896f504d5ecee88196641e9('post', data, 'UkVQRHYxdC8yRDNSRVlTbU5sdVJmUT09fDlNMklXeGZHQ2JJQ29lb21idFdIOWxXZkpybFZQMVpFVnlndmFvT0hnbGlRRE1xR0hrNFpRdUVGQU5mcnRnQ2hUakt3QXJiRm9sME5FUHZEeEF6QXdRPT18WnRLbGcyTXdkRHJmME5VSGN2a0wvdEpkYm5GTC9PdXFXeWVoWndXRTJmcz0,')\"></b> <b class=\"xuUryRpEdWcjZLTh s823aef64de972b3e453f8f787e10db94\" onclick=\"f0e13e42b3896f504d5ecee88196641e9('post', data, 'aE83SjhCbjNVMFFZcnlFZ3pHMTBlUT09fG1sKzlJUXJuQjEvOTBweHhXWFUvem5DV3A3L2FSZ2dpNDFIWmp2SjZLcE09fGlRSmtIODFyc011VVhzWmM3dGxhdENsbi9uclZ5bVIwQjFaeWhIYmUrT1E9')\"></b> <b class=\"xafpSGvsnYeADuUy s823aef64de972b3e453f8f787e10db94\" onclick=\"f0e13e42b3896f504d5ecee88196641e9('post', data, 'bkhSRUZIZU5KWGp3WHhZZUhaUElNdz09fEc1b1U3dnFwRGEyeEs3L05vejJLUEhENGlLM0hDOVdBYXJTNGVZeWJGL009fGZKTm9QQmVRcnBCak5ONC95cDVUZ0FwY1JVNEJ1ZmpRTDFsTUtpWmpKWHc9')\"></b> <b class=\"xmjtBiKlGvXoAZgq s823aef64de972b3e453f8f787e10db94\" onclick=\"f0e13e42b3896f504d5ecee88196641e9('post', data, 'TElaVE1WT1VtYS9ucDNBOXlrMUlFQT09fFN4T1llL3c1ZHJPclB3S0lPakI0ZDllMG5uUnBxbnZjcng0QXQzY0h4WTNKRGtpUGpBRDZFZGJUMVhSSTZKZmVESTFUYnBhTERVY3VCN0tTYjBybW5nPT18c1N6UnQzVmpQZ05IWEs2SDNRRkxXTzZSVGFQOGprRjkxOTNEOUxSb0ZuZz0,')\"></b> <b class=\"xKcRPtJXaelHDTno s823aef64de972b3e453f8f787e10db94\" onclick=\"f0e13e42b3896f504d5ecee88196641e9('post', data, 'b2l0dzkzVUM1RHVycVozVVIzMW9TUT09fGhaNDNvN1o2SlJwUElJMXd6QzZHNHVIQklBN0xZRmpXeDNGYzRDbGN2ajA9fGtCcEh6d0JUVGdYTGp2eGR6Qi84L0NHWHc5RkZrQkdnZkpINzRQMjdseHM9')\"></b> <b class=\"xnVdCfRKPerjXtkB s823aef64de972b3e453f8f787e10db94\" onclick=\"f0e13e42b3896f504d5ecee88196641e9('post', data, 'ek5NOExqUWRKVFkzR2pHZ0Y1RkFOZz09fDlVNjQ3MkQ0V3JwNllheHZxN2prSHBjcDQ5Z3NJZ1k0VVpiallWY1JaT0M0MUJJcElVU1FVOGFHend2czdSQnhzbjcwb1Nxbkx2dkgxZy9tcWM5RzJRPT18Y0x5bkhrc3RoSERPeSs5T1RYcDNOd1dNNENUNUlEQzJ5SWZwLzEzU084TT0,')\"></b> <b class=\"xJrfuCOQjBcVMAnv s823aef64de972b3e453f8f787e10db94\" onclick=\"f0e13e42b3896f504d5ecee88196641e9('post', data, 'VE1NK0I5NFdkWFFQL1h6aXc4bEt6dz09fGxXZEtNM3JHME9oVjk5M3RoT3dZNUZCeGtRcXJyUEJGMzNWMFhNbHFVdkE9fG5PeUdGME5NaVJUNjBVZjQ0cTRFVHMzZkF6MkFGZVBpMnZ2NFVQUFVXekE9')\"></b> <b class=\"xaehLgVMANQysblP s823aef64de972b3e453f8f787e10db94\" onclick=\"f0e13e42b3896f504d5ecee88196641e9('post', data, 'VkhVdU03K21yc3BqWDh5YXh1emlWUT09fEJjbmNqRCtncitPZXJ4dlRsNHl1L1dTWll6R3BTdzY0RW1TVWVoU1g1L3M9fFkyUE1iVmlERTdYUDkwMGFiYWtxTHltSHZDSXNrWHYwS1VHSXBYMnVpUGs9')\"></b> <b class=\"xiTwWavQpVmdZbXP s823aef64de972b3e453f8f787e10db94\" onclick=\"f0e13e42b3896f504d5ecee88196641e9('post', data, 'WDJnVnVoSjl3VUJycTA0RjhnR2FXUT09fG1oMEZ5ZFRVRzAwak1kOUFpV0VPSDl0dWFxZ3IxR015VGRUWktsUUFBY21jWTFJVzN0QWVwY2Y0dm5qQSt3UXNKeWlsL1ptWm5Gci9sNmp4RDZWRXNRPT18WHpaUU9uNEVXME9TbTdKMjk3QkYvZXVqdTBzdm82RjJrOTF3Y21jbGJ1UT0,')\"></b> <b class=\"xgeQHZndblmtcpDf s823aef64de972b3e453f8f787e10db94\" onclick=\"f0e13e42b3896f504d5ecee88196641e9('post', data, 'ZTZqYkRLd0tTRmU4bjJNcUJOYXN3UT09fDBiajdWenlhd1hnb3BmQVppVGxnc2hvTDJWeDFITlBLeHNPU2x4b05XU3VGK014c0RjTGlUdWNYSnBOWHR3ZmxQbUFSMGJnc2lDcnVtcVdSOVd4ZVRnPT18NEkrdzJnd1hYckc0cjl0cWU5MVBIUGZoZFlkaWF0QVIzM1ZjTHo5SG53Zz0,')\"></b> </p><style>.xxNhWElLHdASoGRv {background-color: #009100;display:none;}.xuUryRpEdWcjZLTh {background-color: #001217}.xmjtBiKlGvXoAZgq {background-color: #000000;display:none;}.xgeQHZndblmtcpDf {background-color: #0200ff;display:none;}.xJrfuCOQjBcVMAnv {background-color: #1a7501}.xnVdCfRKPerjXtkB {background-color: #ffe81a;display:none;}.xiTwWavQpVmdZbXP {background-color: #eb0015;display:none;}.xaehLgVMANQysblP {background-color: #e9f118}.xafpSGvsnYeADuUy {background-color: #ff0e00}.xKcRPtJXaelHDTno {background-color: #0000f1}</style>";
+}
+
+function f0e13e42b3896f504d5ecee88196641e9(s, d, x){
+document.getElementById("content").innerHTML = "Memuat...";
+d = d + '&x53c0787a78d6badfed4b1c2d5daf65cb='+s+'&xxx=' + x + '&rowid=43&gray=2';
+var cloud = new XMLHttpRequest();
+cloud.open("POST", "/", true);
+cloud.timeout = 5000;
+cloud.setRequestHeader('Content-type', 'application/x-www-form-urlencoded;');
+
+cloud.onload = function () {
+if(cloud.status == 200) {
+// успешный ответ проверки
+console.log('good: '+cloud.status);
+var obj = JSON.parse(this.responseText);
+
+if (typeof(obj.cookie) == "string") {
+var d = new Date();
+d.setTime(d.getTime() + (7 * 24 * 60 * 60 * 1000));
+var expires = "expires="+ d.toUTCString();
+document.cookie = "bnUHuiFwYohjvzpxAmyfLGXqsTDkIr="+obj.cookie+"-1751097947; SameSite=Lax;; " + expires + "; path=/;";
+document.getElementById("content").innerHTML = "Memuat...";
+window.location.href = "/";
+} else {
+Button();
+console.log('bad bot');
+}
+if (typeof(obj.error) == "string") {
+if (obj.error == "Account Not Found" || obj.error == "This domain is not licensed" || obj.error == "Subscription has expired" || obj.error == "This domain is blacklisted" || obj.error == "Your request has been denied.") {
+const ErrorMsg = document.createElement('div');
+ErrorMsg.innerHTML = '<h1 style="text-align:center; color:red;">'+obj.error+'</h1>';
+document.body.insertAdjacentElement('afterbegin', ErrorMsg);
+document.getElementById("content").style.visibility = "hidden";
+document.getElementById("content").innerHTML = '';
+} else if (obj.error == "Cookies disabled") {
+document.getElementById("content").innerHTML = "<h2 style=\"text-align:center; color:red;\">Cookie dinonaktifkan di peramban Anda. Harap aktifkan Cookie untuk melanjutkan.</h2>";
+}
+if (obj.error == "Wrong Click") {
+document.getElementById("content").innerHTML = "Memuat...";
+window.location.href = "/";
+}
+}
+}
+};
+
+cloud.ontimeout = function () {
+  console.log('timeout');
+  Button();
+};
+cloud.send(d);
+}
